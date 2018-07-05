f400d14ba48da7db5d2018c3eb2d9c89ca716426
Devam6316015 <makkar.devam@gmail.com>
2018-07-05 15:03:10 +0530

    complete

--- assign 15 regular expression(A).py
+++ assign 15 regular expression(A).py
@@ -9,10 +9,6 @@ p=re.compile(r"([a-zA-Z0-9]+)@([A-Za-z0-9]+).([a-z]+)")
 result=p.findall(emails)
 print(result)
 
-p=re.compile(r"($@.\\b)")
-result=p.find(emails)
-
-print(result.group())
 
 
 
@@ -39,3 +35,51 @@ t3=(result.group(7),result.group(8),result.group(9))
 
 l=[t1,t2,t3]
 print(l)
+
+
+
+
+
+#Q.2- Retrieve all the words starting with ‘b’ or ‘B’ from the following text.
+
+import re
+
+text = "Betty bought a bit of butter, But the butter was so bitter, So she bought some better butter, To make the bitter butter better."
+
+p=re.compile(r"\b[bB]\w+")
+
+result=p.findall(text)
+
+for r in result:
+	print(r)
+	
+	
+	
+	
+#Q.3- Split the following irregular sentence into words 
+
+import re
+
+sentence = "A, very very; irregular_sentence"
+
+p=re.compile(r"[^A-Za-z0-9]")
+result=p.sub(" ",sentence)
+ 
+print(result)
+
+
+
+
+
+#Q.1- Clean up the following tweet so that it contains only the user’s message. 
+#That is, remove all URLs, hashtags, mentions, punctuations, RTs and CCs. 
+
+tweet = 'Good advice! RT @TheNextWeb: What I would do differently if I was learning to code today http://t.co/lbwej0pxOd cc: @garybernhardt #rstats'
+#desired_output = 'Good advice What I would do differently if I was learning to code today'
+
+import re
+
+p=re.compile(r"(.+)! (.+): (.+) http(.+)")
+result=p.match(tweet)
+ 
+print(result.group(1),result.group(3))
